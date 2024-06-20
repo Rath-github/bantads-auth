f8163016c32e6a9b369081be68796a2685a598c4
IamThiago-IT <thiagodossantos315@gmail.com>
2024-06-20 20:17:23 -0300

    feat: add files

---
+++ src/main/java/com/ufpr/bantadsauth/BantadsAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ufpr.bantadsauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BantadsAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BantadsAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufpr/bantadsauth/BantadsAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufpr.bantadsauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BantadsAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
